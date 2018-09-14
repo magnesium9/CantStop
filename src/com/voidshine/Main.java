67854da3d4606a414285670db4ee86b512e8f947
zimoyoyo <43260751+zimoyoyo@users.noreply.github.com>
2018-09-13 22:33:03 -0700

    Print "I love you" message
    I added one line to test the environment!

--- src/com/voidshine/Main.java
+++ src/com/voidshine/Main.java
@@ -3,6 +3,8 @@ package com.voidshine;
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Can't Stop!");
+        System.out.println("Can't Stop Love You!");
     }
 }
